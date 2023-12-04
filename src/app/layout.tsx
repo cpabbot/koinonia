4e868ebd19a343c540e6d784122e2aed696f2ae8
Cameron Abbot <cpabbot1@gmail.com>
2023-12-03 19:47:46 -0500

    initial nextjs app created

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Montserrat, Jacques_Francois } from "next/font/google";
+import "./globals.css";
+
+const mont = Montserrat({ subsets: ["latin"] });
+
+export const jacques = Jacques_Francois({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-jacques",
+});
+
+export const metadata: Metadata = {
+  title: "Koinonia",
+  description: "A full-stack web app built with NextJS and deployed with aws.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={jacques.variable}>
+      <body className={mont.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Koinonia</h1>
+      <span>A full-stack web app built with NextJS and deployed with aws.</span>
+    </main>
+  )
+}
