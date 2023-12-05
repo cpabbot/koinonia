9f02adb9a1c74951f381049f47007b1b66ae622f
Cameron Abbot <cpabbot1@gmail.com>
2023-12-04 21:00:30 -0500

    removed deploy yml again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Koinonia</h1>
-      <span>A full-stack web app built with NextJS and deployed with aws.</span>
+      <span>A full-stack web app built with NextJS and deployed with AWS.</span>
     </main>
   )
 }
